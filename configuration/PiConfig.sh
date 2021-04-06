9c1598a6e6166b2bfc6835aa3b41eeca9cdd17d8
Yohan Bélanger <bely2803@usherbrooke.ca>
2021-04-05 20:51:58 -0400

    Added python installation in PiConfig.sh

--- configuration/PiConfig.sh
+++ configuration/PiConfig.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 apt-get update
-apt-get install 
+
+#Python
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+sudo tar zxf Python-3.7.0.tgz
+cd Python-3.7.0
+sudo ./configure
+sudo make -j 4
+sudo make altinstall
+
+# Libraries
+apt-get install
 pip3 install PyQt5
 pip3 install pyserial
 pip3 install gpiozero
 pip3 install inquirer
-pip install RPi.GPIO
+pip3 install RPi.GPIO
